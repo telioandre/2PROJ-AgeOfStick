ca50241b40887feee21e012ab0af64bc7c764cda
moutoussamy-oriane <118806763+OrianeMoutoussamy@users.noreply.github.com>
2024-03-28 13:58:45 +0100

    Option Son qui marche
    - Ajout de Mixer
    - Ajout de Scripts
    - Slider qui marchent

---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public TMPro.TMP_Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        for (int i=0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+        }
+
+        resolutionDropdown.AddOptions(options);
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
---
+++ Assets/SetVolume.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SetVolume : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    public void SetLevel(float volume)
+    {
+        mixer.SetFloat("volume", volume);
+    }
+}
